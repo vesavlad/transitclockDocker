03b9bf53e216e61033d960f7bad6e2b5e8cad5ef
scrudden <og.crudden@gmail.com>
2017-12-12 18:18:36 +0000

    Forgot to save file. URL for capmetro on github set to raw.

--- bin/process_avl.sh
+++ bin/process_avl.sh
@@ -20,42 +20,42 @@ psql \
 	
 # This will process AVL using the set of transitime config files in the test directory.
 #find /usr/local/transitime/config/test/ -type f | sort -n | xargs -I {} java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles={} -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0 \;
-wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-13.csv -O /tmp/data.csv
+wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-13.csv?raw=true -O /tmp/data.csv
 java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig1.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
 java -Xmx2048m -Xss12m -Dnet.sf.ehcache.disabled=false -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig1.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/UpdateTravelTimes.jar 11-13-2017 11-13-2017
 
-wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-14.csv -O /tmp/data.csv
+wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-14.csv?raw=true -O /tmp/data.csv
 java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig2.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
 java -Xmx2048m -Xss12m -Dnet.sf.ehcache.disabled=false -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig2.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/UpdateTravelTimes.jar 11-13-2017 11-14-2017
 
-wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-15.csv -O /tmp/data.csv
+wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-15.csv?raw=true -O /tmp/data.csv
 java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig3.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
 java -Xmx2048m -Xss12m -Dnet.sf.ehcache.disabled=false -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig3.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/UpdateTravelTimes.jar 11-13-2017 11-15-2017
 
-wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-16.csv -O /tmp/data.csv
+wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-16.csv?raw=true -O /tmp/data.csv
 java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig4.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
 java -Xmx2048m -Xss12m -Dnet.sf.ehcache.disabled=false -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig4.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/UpdateTravelTimes.jar 11-13-2017 11-16-2017
 
-wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-17.csv -O /tmp/data.csv
+wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-17.csv?raw=true -O /tmp/data.csv
 java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig4.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
 java -Xmx2048m -Xss12m -Dnet.sf.ehcache.disabled=false -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig4.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/UpdateTravelTimes.jar 11-13-2017 11-17-2017
 
 ## RUN for another week without updates to travel times.
-wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-20.csv -O /tmp/data.csv
+wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-20.csv?raw=true -O /tmp/data.csv
 java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig4.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
 
-wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-21.csv -O /tmp/data.csv
-java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig4.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
-
-
-wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-22.csv -O /tmp/data.csv
+wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-21.csv?raw=true -O /tmp/data.csv
 java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig4.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
 
 
-wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-23.csv -O /tmp/data.csv
+wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-22.csv?raw=true -O /tmp/data.csv
+java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig4.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
+2017-11-10.csv
+
+wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-23.csv?raw=true -O /tmp/data.csv
 java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig4.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
 
-wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-24.csv -O /tmp/data.csv
+wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-24.csv?raw=true -O /tmp/data.csv
 java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig4.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
 
 
