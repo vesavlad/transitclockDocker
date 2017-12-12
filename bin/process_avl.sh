576f1c6ac5361384e10ec471db76c34235d376a2
scrudden <og.crudden@gmail.com>
2017-12-12 17:53:35 +0000

    Setting up CAPMETRO 10 day test run.

--- bin/process_avl.sh
+++ bin/process_avl.sh
@@ -20,14 +20,42 @@ psql \
 	
 # This will process AVL using the set of transitime config files in the test directory.
 #find /usr/local/transitime/config/test/ -type f | sort -n | xargs -I {} java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles={} -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0 \;
+wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-13.csv -O /tmp/data.csv
 java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig1.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
-java -Xmx2048m -Xss12m -Dnet.sf.ehcache.disabled=false -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig1.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/UpdateTravelTimes.jar 07-10-2017 07-10-2017
+java -Xmx2048m -Xss12m -Dnet.sf.ehcache.disabled=false -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig1.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/UpdateTravelTimes.jar 11-13-2017 11-13-2017
 
+wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-14.csv -O /tmp/data.csv
 java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig2.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
-java -Xmx2048m -Xss12m -Dnet.sf.ehcache.disabled=false -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig2.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/UpdateTravelTimes.jar 07-10-2017 07-11-2017
+java -Xmx2048m -Xss12m -Dnet.sf.ehcache.disabled=false -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig2.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/UpdateTravelTimes.jar 11-13-2017 11-14-2017
 
+wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-15.csv -O /tmp/data.csv
 java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig3.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
-java -Xmx2048m -Xss12m -Dnet.sf.ehcache.disabled=false -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig3.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/UpdateTravelTimes.jar 07-10-2017 07-12-2017
+java -Xmx2048m -Xss12m -Dnet.sf.ehcache.disabled=false -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig3.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/UpdateTravelTimes.jar 11-13-2017 11-15-2017
 
+wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-16.csv -O /tmp/data.csv
 java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig4.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
-java -Xmx2048m -Xss12m -Dnet.sf.ehcache.disabled=false -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig4.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/UpdateTravelTimes.jar 07-10-2017 07-13-2017
+java -Xmx2048m -Xss12m -Dnet.sf.ehcache.disabled=false -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig4.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/UpdateTravelTimes.jar 11-13-2017 11-16-2017
+
+wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-17.csv -O /tmp/data.csv
+java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig4.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
+java -Xmx2048m -Xss12m -Dnet.sf.ehcache.disabled=false -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig4.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/UpdateTravelTimes.jar 11-13-2017 11-17-2017
+
+## RUN for another week without updates to travel times.
+wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-20.csv -O /tmp/data.csv
+java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig4.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
+
+wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-21.csv -O /tmp/data.csv
+java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig4.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
+
+
+wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-22.csv -O /tmp/data.csv
+java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig4.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
+
+
+wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-23.csv -O /tmp/data.csv
+java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig4.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
+
+wget https://github.com/scascketta/CapMetrics/blob/master/data/vehicle_positions/2017-11-24.csv -O /tmp/data.csv
+java -Xmx2048m -Xss12m -Duser.timezone=EST -Dtransitime.configFiles=/usr/local/transitime/config/test/transiTimeConfig4.xml -Dtransitime.core.agencyId=$AGENCYID -Dtransitime.logging.dir=/usr/local/transitime/logs/ -jar $TRANSITIMECORE/transitime/target/Core.jar -configRev 0
+
+
--- go.sh
+++ go.sh
@@ -10,10 +10,10 @@ docker rmi transitime-server
 
 docker build -t transitime-server \
 --build-arg TRANSITIME_GITHUB="https://github.com/TheTransitClock/transitime.git" \
---build-arg TRANSITIME_BRANCH="VIA" \
---build-arg AGENCYNAME=GOHART \
---build-arg GTFS_URL="http://gohart.org/google/google_transit.zip" \
---build-arg GTFSRTVEHICLEPOSITIONS="http://realtime.prod.obahart.org:8088/vehicle-positions" .
+--build-arg TRANSITIME_BRANCH="barefoot" \
+--build-arg AGENCYNAME=CAPMETRO \
+--build-arg GTFS_URL="https://data.texas.gov/download/r4v4-vz24/application%2Fzip" \
+--build-arg GTFSRTVEHICLEPOSITIONS="https://data.texas.gov/download/eiei-9rpf/application%2Foctet-stream" .
 
 docker run --name transitime-db -p 5432:5432 -e POSTGRES_PASSWORD=$PGPASSWORD -d postgres
 
@@ -27,8 +27,6 @@ docker run --name transitime-server-instance --rm --link transitime-db:postgres 
 
 docker run --name transitime-server-instance --rm --link transitime-db:postgres -e PGPASSWORD=$PGPASSWORD transitime-server ./create_webagency.sh
 
-docker run --name transitime-server-instance --rm --link transitime-db:postgres -e PGPASSWORD=$PGPASSWORD transitime-server ./import_avl.sh
-
 docker run --name transitime-server-instance --rm --link transitime-db:postgres -e PGPASSWORD=$PGPASSWORD transitime-server ./process_avl.sh
 
-#docker run --name transitime-server-instance --rm --link transitime-db:postgres -e PGPASSWORD=$PGPASSWORD  -p 8080:8080 transitime-server  ./start_transitime.sh
+docker run --name transitime-server-instance --rm --link transitime-db:postgres -e PGPASSWORD=$PGPASSWORD  -p 8080:8080 transitime-server  ./start_transitime.sh
