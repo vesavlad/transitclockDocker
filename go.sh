754c7d27012191d1f0d2bee6fb648941289a5882
Sean Og Crudden <og.crudden@gmail.com>
2018-09-08 12:48:43 +0000

    Make sure latest saved.

--- go.sh
+++ go.sh
@@ -1,12 +1,12 @@
 export PGPASSWORD=transitclock
 
-docker stop transitclock-db
-docker stop transitclock-server-instance
+#docker stop transitclock-db
+#docker stop transitclock-server-instance
 
-docker rm transitclock-db
-docker rm transitclock-server-instance
+#docker rm transitclock-db
+#docker rm transitclock-server-instance
 
-docker rmi transitclock-server
+#docker rmi transitclock-server
 
 docker build --no-cache -t transitclock-server \
 --build-arg TRANSITCLOCK_PROPERTIES="config/transitclockConfig.xml" \
@@ -15,20 +15,20 @@ docker build --no-cache -t transitclock-server \
 --build-arg GTFS_URL="https://cdn.mbta.com/MBTA_GTFS.zip" \
 --build-arg GTFSRTVEHICLEPOSITIONS="https://data.texas.gov/download/eiei-9rpf/application%2Foctet-stream" .
 
-docker run --name transitclock-db -p 5432:5432 -e POSTGRES_PASSWORD=$PGPASSWORD -d postgres:9.6.3
+docker run --name transitclock-db-instance-2 -p 5433:5432 -e POSTGRES_PASSWORD=$PGPASSWORD -d postgres:9.6.3
 
-docker run --name transitclock-server-instance --rm --link transitclock-db:postgres -e PGPASSWORD=$PGPASSWORD transitclock-server check_db_up.sh
+docker run --name transitclock-server-instance-2 --rm --link transitclock-db-instance-2:postgres -e PGPASSWORD=$PGPASSWORD transitclock-server check_db_up.sh
 
-docker run --name transitclock-server-instance --rm --link transitclock-db:postgres -e PGPASSWORD=$PGPASSWORD transitclock-server create_tables.sh
+docker run --name transitclock-server-instance-2 --rm --link transitclock-db-instance-2:postgres -e PGPASSWORD=$PGPASSWORD transitclock-server create_tables.sh
 
-docker run --name transitclock-server-instance --rm --link transitclock-db:postgres -e PGPASSWORD=$PGPASSWORD transitclock-server import_gtfs.sh
+docker run --name transitclock-server-instance-2 --rm --link transitclock-db-instance-2:postgres -e PGPASSWORD=$PGPASSWORD transitclock-server import_gtfs.sh
 
-docker run --name transitclock-server-instance --rm --link transitclock-db:postgres -e PGPASSWORD=$PGPASSWORD transitclock-server create_api_key.sh
+docker run --name transitclock-server-instance-2 --rm --link transitclock-db-instance-2:postgres -e PGPASSWORD=$PGPASSWORD transitclock-server create_api_key.sh
 
-docker run --name transitclock-server-instance --rm --link transitclock-db:postgres -e PGPASSWORD=$PGPASSWORD transitclock-server create_webagency.sh
+docker run --name transitclock-server-instance-2 --rm --link transitclock-db-instance-2:postgres -e PGPASSWORD=$PGPASSWORD transitclock-server create_webagency.sh
 
 #docker run --name transitclock-server-instance --rm --link transitclock-db:postgres -e PGPASSWORD=$PGPASSWORD transitclock-server ./import_avl.sh
 
 #docker run --name transitclock-server-instance --rm --link transitclock-db:postgres -e PGPASSWORD=$PGPASSWORD transitclock-server ./process_avl.sh
 
-docker run --name transitclock-server-instance --rm --link transitclock-db:postgres -e PGPASSWORD=$PGPASSWORD  -p 8080:8080 transitclock-server  start_transitclock.sh
+docker run --name transitclock-server-instance-2 --rm --link transitclock-db-instance-2:postgres -e PGPASSWORD=$PGPASSWORD  -p 8081:8080 transitclock-server  start_transitclock.sh
