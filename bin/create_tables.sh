127f328452c461e7e871232b51ed7c4dc407f03d
Vlad Vesa <vvesa@idirect.net>
2021-07-22 11:33:49 +0300

    fixed faulty route type

--- bin/create_tables.sh
+++ bin/create_tables.sh
@@ -5,12 +5,15 @@ java -cp /usr/local/transitclock/Core.jar org.transitclock.applications.SchemaGe
 java -cp /usr/local/transitclock/Core.jar org.transitclock.applications.SchemaGenerator -p org.transitclock.db.webstructs -o /usr/local/transitclock/db
 
 createdb -h $POSTGRES_PORT_5432_TCP_ADDR -p $POSTGRES_PORT_5432_TCP_PORT -U postgres $AGENCYNAME
+
+sed 's/type varchar(2),/type varchar(5),/g' /usr/local/transitclock/db/ddl_postgres_org_transitclock_db_structs.sql > /usr/local/transitclock/db/ddl_postgres_org_transitclock_db_structs_fixed.sql
+
 psql \
 	-h $POSTGRES_PORT_5432_TCP_ADDR \
 	-p $POSTGRES_PORT_5432_TCP_PORT \
 	-U postgres \
 	-d $AGENCYNAME \
-	-f /usr/local/transitclock/db/ddl_postgres_org_transitclock_db_structs.sql
+	-f /usr/local/transitclock/db/ddl_postgres_org_transitclock_db_structs_fixed.sql
 psql \
 	-h $POSTGRES_PORT_5432_TCP_ADDR \
 	-p $POSTGRES_PORT_5432_TCP_PORT \
